1cee3f077897ebdd093ba8ca50b86bd74c0db2dd
Bernhard Froemel <froemel@vmars.tuwien.ac.at>
2013-04-21 22:09:19 +0200

    reduced max block size, hopefully doesn't break anything vital. thanks: https://bitcointalk.org/index.php?topic=182430.msg1904697#msg1904697

--- src/main.h
+++ src/main.h
@@ -26,7 +26,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const unsigned int MAX_BLOCK_SIZE = 1000000;
+static const unsigned int MAX_BLOCK_SIZE = 25000; // by advice of Balthazar, reduced
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
 static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
