9ba49d72163b4775fe48c48dc254eaed424d1473
Bernhard Froemel <froemel@vmars.tuwien.ac.at>
2013-05-05 20:51:47 +0200

    returned to 120 required blocks for maturity (no need to be fast here for micro transactions)

--- src/main.h
+++ src/main.h
@@ -43,7 +43,7 @@ static const int64 MIN_TX_FEE = 10000000;
 static const int64 MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64 MAX_MONEY = 420699680 * COIN; // maximum of 420 699 680 coins
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
-static const int COINBASE_MATURITY = 10;
+static const int COINBASE_MATURITY = 100;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 #ifdef USE_UPNP
